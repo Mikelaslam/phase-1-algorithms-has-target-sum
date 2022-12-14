9556937a0f07ade4bf1d826061d262354eedd46e
mikelaslam <kipkorir385@gmail.com>
2022-12-15 01:41:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  
+    {
+        for (let i = 0; i < array.length; i++) {
+        // n steps (depending on the length of the input array)
+        const complement = target - array[i];
+        for (let j = i + 1; j < array.length; j++) {
+          // n * n steps (nested loop!)
+          if (array[j] === complement) return true;
+        }
+      }
+      // 1
+      return false;
+    }
+    
+  }
+  /* Write your algorithm here
+1. Initialize a function called has targetSum
+2. Define a variable comliment = target- array[i];
+3. By implementing if and if else we achieve our target
 
 /* 
   Write the Big O time complexity of your function here
-*/
-
+Check if the array passed have two numbers that can add up to the target sum
+If the item passed has two numbers adding up to the target sum, return true
+If the array passed do not have numbers adding up to the target sum, return false
 /* 
   Add your pseudocode here
-*/
-
+Declare a function hasTargetSum that receives 2 arguments; an array of integers and a target sum
+Define a variable compliment which equals (target- array lenght)
+Iterate over each item in the array
+If the array has a target sum it will return true, if it doesn't it will return false.
 /*
-  Add written explanation of your solution here
-*/
+/*Add written explanation of your solution here
+After initializing the functionan declaring the variables, we go ahead and implement the if, else and
+ else if functions to achieve the target/*
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
